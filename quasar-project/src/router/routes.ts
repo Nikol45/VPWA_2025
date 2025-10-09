9fc017cd9fcc09f08893b4a482ebce1d37b27015
Nikol Maljarová <123827916+Nikol45@users.noreply.github.com>
2025-10-10 01:52:08 +0200

    Updated project structure
    - added Pinia
    - created home and auth layouts
    - created pages
    - created routes

--- quasar-project/src/router/routes.ts
+++ quasar-project/src/router/routes.ts
@@ -1,10 +1,30 @@
 import type { RouteRecordRaw } from 'vue-router';
 
-const routes: RouteRecordRaw[] = [
-  {
+const routes: RouteRecordRaw[] = [{
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    component: () => import('layouts/AuthLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/AuthPage.vue') },
+      { path: 'register', component: () => import('pages/RegisterPage.vue') }
+    ],
+  },
+
+  {
+    path: '/home/:id',
+    component: () => import('layouts/HomeLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/HomePage.vue') },
+      { path: 'settings', component: () => import('pages/ProfileSettings.vue') }
+    ],
+  }, 
+  
+  {
+    path: 'channel/:id', 
+    component: () => import('layouts/HomeLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/Chat.vue') },
+      { path: 'settings', component: () => import('pages/ChannelSettings.vue') }
+    ],
   },
 
   // Always leave this as last one,
